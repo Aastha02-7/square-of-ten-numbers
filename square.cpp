833faaa1b891dec37004fd73298142a95913b5d2
Aastha02-7 <53691699+Aastha02-7@users.noreply.github.com>
2019-11-07 21:06:07 +0700

    Add files via upload

---
+++ square.cpp
@@ -0,0 +1,14 @@
+#include<conio.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i=1,sqr;
+	while(i<11)
+	{
+		sqr=i*i;
+		cout<<"Square of "<<i<<" is "<<sqr<<endl;
+		i++;
+	}
+	return 0;
+}
